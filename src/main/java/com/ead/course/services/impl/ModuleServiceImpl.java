a0f5723e1c8e7f9ed253fd2006bcb7065ffe8745
Erich Silveira <erichsilveira@Erichs-MBP.localdomain>
2023-07-24 09:09:42 -0300

    module and entity management

--- src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
@@ -1,17 +1,35 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
+import com.ead.course.repositories.LessonRepository;
 import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.services.ModuleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
 @Service
 public class ModuleServiceImpl implements ModuleService {
 
     ModuleRepository moduleRepository;
+    LessonRepository lessonRepository;
 
     @Autowired
-    public ModuleServiceImpl(ModuleRepository moduleRepository) {
+    public ModuleServiceImpl(ModuleRepository moduleRepository, LessonRepository lessonRepository) {
         this.moduleRepository = moduleRepository;
+        this.lessonRepository = lessonRepository;
+    }
+
+    @Transactional
+    @Override
+    public void delete(ModuleModel moduleModel) {
+        List<LessonModel> lessonModelList = lessonRepository.findAllLessonsIntoModel(moduleModel.getModuleId());
+        if (!lessonModelList.isEmpty()) {
+            lessonRepository.deleteAll(lessonModelList);
+        }
+        moduleRepository.delete(moduleModel);
     }
 }
